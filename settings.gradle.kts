6e41ddc153d2368979ce087fcee9e2989b09cca6
GitHub Action <action@github.com>
2023-03-03 19:30:37 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "intellij-my-custom-theme"
--- src/main/kotlin/com/github/mty95/intellijmycustomtheme/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mty95/intellijmycustomtheme/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mty95.intellijmycustomtheme.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mty95.intellijmycustomtheme.MyBundle
 
 class MyApplicationService {
 
